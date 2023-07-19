a4e74cb2a558fb1506dbc80ddcbbe79403e13acd
keisuke-fukuchi <keisuke_fukuchi@r.recruit.co.jp>
2023-07-19 11:39:40 +0900

    add: hello template

--- src/main/java/jp/gihyo/pro/java/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/pro/java/tasklist/HomeRestController.java
@@ -1,6 +1,7 @@
 package jp.gihyo.pro.java.tasklist;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -13,18 +14,9 @@ import java.util.stream.Collectors;
 @RequestMapping("/rest")
 public class HomeRestController {
     @RequestMapping(value = "/hello")
-    @ResponseBody
-    String hello() {
-        return """
-                <html>
-                   <head><title>Hello</title></head>
-                   <body>
-                     <h1>Hello</h1>
-                     It works!<br>
-                     現在時刻は%sです。
-                   </body>
-                </html>
-                """.formatted(LocalDateTime.now());
+    String hello(Model model) {
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 
     record TaskItem(String id, String task, String deadline, boolean done) {}
